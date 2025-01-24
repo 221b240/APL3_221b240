13b1dfece15a4e022620cff212f723709cd87877
221B240 <221B240@juet.co.in>
2025-01-24 14:43:10 +0530

    Lab exercise 1

---
+++ LE1/Problem 2/Child (1).java
@@ -0,0 +1,5 @@
+class Child extends Mother {
+    void show(int x) {
+        System.out.println("this is function of child class and value of x is " + x);
+    }
+}
---
+++ LE1/Problem 2/Mother (1).java
@@ -0,0 +1,6 @@
+class Mother {
+    int x;
+    void show(int x) {
+        System.out.println("value of x is " + x);
+    }
+}
